29b2837f932e43f0790bf6ed0c95a93202b9aa4f
Serwan Asaad <serwan.asaad@quantum-machines.co>
2023-07-13 09:08:50 +0200

    remvoe controller

--- quam_components/core/quam_base.py
+++ quam_components/core/quam_base.py
@@ -4,11 +4,8 @@ from .qua_config import build_config
 
 
 class QuamBase:
-    controller: str = "con1"
-
     def __init__(self, filepath):
         self.filepath = filepath
-
 
     def build_config(self):
         return build_config(self)
