63bfc7827ad1bc7933409eed7d3e2df15bfbb809
Serwan Asaad <serwan.asaad@quantum-machines.co>
2023-07-13 21:25:56 +0200

    test nested iteration

--- quam_components/tests/test_quam_base.py
+++ quam_components/tests/test_quam_base.py
@@ -22,3 +22,34 @@ def test_iterate_quam_elements():
     assert len(elems) == 3
     assert all(isinstance(elem, QuamElement) for elem in elems)
 
+
+@dataclass
+class TestQuamElement(QuamElement):
+    int_val: int
+    quam_elem: QuamElement
+    quam_elem_list: List[QuamElement]
+
+
+@dataclass
+class TestQuamNested(QuamBase):
+    int_val: int
+    quam_elem: TestQuamElement
+    quam_elem_list: List[TestQuamElement]
+
+
+def test_iterate_quam_elements_nested():
+    quam_element = TestQuamElement(
+        int_val=42,
+        quam_elem=QuamElement(),
+        quam_elem_list=[QuamElement(), QuamElement()]
+    )
+
+    test_quam = TestQuam(
+        int_val=42,
+        quam_elem=quam_element,
+        quam_elem_list=[quam_element, quam_element]
+    )
+
+    elems = list(iterate_quam_elements(test_quam))
+    assert len(elems) == 12
+    assert all(isinstance(elem, QuamElement) for elem in elems)
