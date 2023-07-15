3230b0dfa7e1563df57d7825079c416ee99a869f
Serwan Asaad <serwan.asaad@quantum-machines.co>
2023-07-15 10:50:58 +0200

    Close to working QuamDict

--- quam_components/core/quam_instantiation.py
+++ quam_components/core/quam_instantiation.py
@@ -8,7 +8,19 @@ if TYPE_CHECKING:
     from quam_components.core import QuamBase, QuamElement
 
 
-def get_class_attributes(cls: type):
+def get_class_attributes(cls: type) -> Dict[str, List[str]]:
+    """Get the attributes of a class that are not methods or private.
+
+    Args:
+        cls: The class to get the attributes of.
+
+    Returns:
+        A dictionary with the following keys:
+            - "required": A list of the required attributes of the class.
+            - "optional": A list of the optional attributes of the class,
+              i.e. with a default value.
+            - "allowed": A list of all the allowed attributes of the class.
+    """
     annotated_attrs = get_type_hints(cls)
 
     annotated_attrs.pop("_quam", None)
@@ -34,15 +46,39 @@ def get_class_attributes(cls: type):
     return attr_annotations
 
 
-def instantiate_quam_attrs(attrs, contents, obj_name, quam_base: QuamBase):
+def instantiate_quam_dict_attrs(contents: dict, quam_base: QuamBase) -> dict:
+    """Instantiate the attributes of a QuamDict"""
+    from quam_components.core import QuamDictElement
+
+    instantiated_attrs = {}
+    for key, val in contents.items():
+        if isinstance(val, dict):
+            instantiated_val = instantiate_quam_element(QuamDictElement, val, quam_base)
+        elif isinstance(val, list):
+            instantiated_val = [
+                elem if not isinstance(elem, dict) 
+                else instantiate_quam_element(QuamDictElement, elem, quam_base)
+                for elem in val
+            ]
+        else:
+            instantiated_val = val
+        instantiated_attrs[key] = instantiated_val
+
+    return instantiated_attrs
+
+
+def instantiate_quam_attrs(cls: type, attrs: Dict[str, List[str]], contents: dict, quam_base: QuamBase) -> dict:
     """Instantiate the attributes of a QuamElement or QuamDictElement"""
     # TODO should type checking be performed here or in the classes
     from quam_components.core import QuamElement, QuamDictElement
 
+    if issubclass(cls, QuamDictElement):
+        return instantiate_quam_dict_attrs(contents, quam_base)
+
     instantiated_attrs = {}
     for key, val in contents.items():
         if key not in attrs["allowed"]:
-            raise AttributeError(f"Invalid attribute {key}")
+            raise AttributeError(f"Attribute {key} is not a valid attr of {cls.__name__}")
 
         # TODO improve type checking
         required_type = attrs["allowed"][key]
@@ -54,25 +90,29 @@ def instantiate_quam_attrs(attrs, contents, obj_name, quam_base: QuamBase):
             required_subtype = required_type.args[0]
             if issubclass(required_subtype, QuamElement):
                 instantiated_val = [
-                    instantiate_quam_element(required_subtype, v, quam_base) for v in val
+                    instantiate_quam_element(required_subtype, v, quam_base)
+                    for v in val
                 ]
             else:
                 instantiated_val = val
         elif issubclass(required_type, (dict, QuamDictElement, Dict)):
             instantiated_val = instantiate_quam_element(QuamDictElement, val, quam_base)
-
-
         else:
             instantiated_val = val
 
-        if not isinstance(instantiated_val, str) or not instantiated_val.startswith(':'):
-            try:
-                check_type(instantiated_val, required_type)
-            except TypeCheckError as e:
-                raise TypeError(
-                    f"Wrong type type({key})={type(instantiated_val)} !="
-                    f" {required_type} for '{obj_name}'"
-                ) from e
+        # Do not check type if the value is a reference
+        if isinstance(instantiated_val, str) and instantiated_val.startswith(":"):
+            instantiated_attrs[key] = instantiated_val
+            continue
+
+        # Perform type checking
+        try:
+            check_type(instantiated_val, required_type)
+        except TypeCheckError as e:
+            raise TypeError(
+                f"Wrong type type({key})={type(instantiated_val)} !="
+                f" {required_type} for '{cls.__name__}'"
+            ) from e
 
         instantiated_attrs[key] = instantiated_val
 
@@ -80,16 +120,14 @@ def instantiate_quam_attrs(attrs, contents, obj_name, quam_base: QuamBase):
         attr for attr in attrs["required"] if attr not in instantiated_attrs
     ]
     if missing_attrs:
-        raise AttributeError(
-            f"Missing required attribute {missing_attrs} for {obj_name}"
-        )
+        raise AttributeError(f"Missing required attr {missing_attrs} for {cls.__name__}")
 
     return instantiated_attrs
 
 
 def instantiate_quam_base(quam_base: QuamBase, contents: dict) -> QuamBase:
     """Instantiate a QuamBase from a dict
-    
+
     Args:
         quam_base: QuamBase instance to instantiate
         contents: dict of attributes to instantiate the QuamBase with
@@ -100,7 +138,10 @@ def instantiate_quam_base(quam_base: QuamBase, contents: dict) -> QuamBase:
     attr_annotations = get_class_attributes(cls=quam_base.__class__)
 
     instantiated_attrs = instantiate_quam_attrs(
-        attr_annotations, contents, obj_name=quam_base.__class__.__name__, quam_base=quam_base
+        cls=quam_base.__class__,
+        attrs=attr_annotations,
+        contents=contents,
+        quam_base=quam_base,
     )
 
     for attr, val in instantiated_attrs.items():
@@ -109,11 +150,13 @@ def instantiate_quam_base(quam_base: QuamBase, contents: dict) -> QuamBase:
     return quam_base
 
 
-def instantiate_quam_element(quam_element_cls: type[QuamElement], contents: dict, quam_base: QuamBase): -> QuamElement:
+def instantiate_quam_element(
+    quam_element_cls: type[QuamElement], contents: dict, quam_base: QuamBase
+) -> QuamElement:
     """Instantiate a QuamElement from a dict
 
     Note that any nested QuamElements are instantiated recursively
-    
+
     Args:
         quam_element_cls: QuamElement class to instantiate
         contents: dict of attributes to instantiate the QuamElement with
@@ -125,7 +168,10 @@ def instantiate_quam_element(quam_element_cls: type[QuamElement], contents: dict
     attr_annotations = get_class_attributes(quam_element_cls)
 
     instantiated_attrs = instantiate_quam_attrs(
-        attr_annotations, contents, obj_name=quam_element_cls.__name__, quam_base=quam_base
+        cls=quam_element_cls,
+        attrs=attr_annotations,
+        contents=contents,
+        quam_base=quam_base,
     )
 
     quam_element = quam_element_cls(**instantiated_attrs)
