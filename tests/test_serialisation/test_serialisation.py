a20f736c25617f3e986fb5b629db9b9dca7c6703
Serwan Asaad <serwan.asaad@quantum-machines.co>
2023-09-05 14:36:18 +0200

    move to single file

--- tests/test_serialisation/test_serialisation.py
+++ tests/test_serialisation/test_serialisation.py
@@ -197,3 +197,54 @@ def test_save_load_folder_content_mapping(tmp_path):
     }
     assert metadata["default_filename"] == "state.json"
     assert metadata["default_foldername"] == str(json_folder)
+
+
+@dataclass
+class TestQuamComponent(QuamComponent):
+    int_val: int
+    inner_quam: QuamComponent = None
+
+
+@dataclass
+class TestQuam(QuamRoot):
+    int_val: int
+    inner_quam: QuamComponent
+
+
+def test_save_explicit_class(tmp_path):
+    test_quam = TestQuamComponent(int_val=42, inner_quam=TestQuamComponent(43))
+
+    json_serialiser = JSONSerialiser()
+    json_file = tmp_path / "quam.json"
+
+    json_serialiser.save(test_quam, path=json_file)
+
+    with open(json_file) as f:
+        contents = json.load(f)
+        assert contents == {
+            "int_val": 42,
+            "inner_quam": {
+                "__class__": "test_serialisation.TestQuamComponent",
+                "int_val": 43,
+            },
+        }
+
+
+def test_save_explicit_class_root(tmp_path):
+    test_quam = TestQuam(int_val=42, inner_quam=TestQuamComponent(43))
+
+    json_serialiser = JSONSerialiser()
+    json_file = tmp_path / "quam.json"
+
+    json_serialiser.save(test_quam, path=json_file)
+
+    with open(json_file) as f:
+        contents = json.load(f)
+        assert contents == {
+            "int_val": 42,
+            "inner_quam": {
+                "__class__": "test_serialisation.TestQuamComponent",
+                "int_val": 43,
+            },
+            "__class__": "test_serialisation.TestQuam",
+        }
