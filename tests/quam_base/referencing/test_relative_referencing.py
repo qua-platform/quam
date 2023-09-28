120bbe943ee18acef69984e58a8aa8e1cfcff7a0
Serwan Asaad <serwan.asaad@quantum-machines.co>
2023-09-28 12:09:00 +0200

    added tests for relative referencing

---
+++ tests/quam_base/referencing/test_relative_referencing.py
@@ -0,0 +1,33 @@
+def test_relative_list_referencing(BareQuamRoot):
+    root = BareQuamRoot()
+    root.l = [1, 2, 3, ":./[0]"]
+    assert root.l[3] == 1
+
+    root.l.append(":../")
+    assert root.l[4] == root
+
+
+def test_relative_dict_referencing(BareQuamRoot):
+    root = BareQuamRoot()
+    root.d = {"a": 1, "b": 2, "c": ":./a"}
+    assert root.d.c == 1
+    assert root.d["c"] == 1
+
+    root.d["d"] = ":../"
+    assert root.d.d == root
+    assert root.d["d"] == root
+
+
+def test_relative_component_referencing(BareQuamRoot, BareQuamComponent):
+    root = BareQuamRoot()
+    root.comp = BareQuamComponent()
+    root.comp.a = 42
+    root.comp.b = ":./a"
+
+    assert root.comp.b == 42
+
+    root.comp.c = ":../"
+    assert root.comp.c == root
+
+    root.comp.d = ":../comp"
+    assert root.comp.d == root.comp
