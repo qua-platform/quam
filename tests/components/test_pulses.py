e84f20cb15df6a3d28d165603342c6d334ae4606
Serwan Asaad <serwan.asaad@quantum-machines.co>
2023-10-07 10:40:10 +0200

    fixed pulse test

--- tests/components/test_pulses.py
+++ tests/components/test_pulses.py
@@ -24,17 +24,6 @@ def test_drag_pulse():
 
     from qualang_tools.config.waveform_tools import drag_gaussian_pulse_waveforms
 
-    assert drag_pulse._get_waveform_kwargs() == {
-        "rotation_angle": 0.0,
-        "amplitude": 1,
-        "sigma": 4,
-        "alpha": 2,
-        "anharmonicity": 200e6,
-        "detuning": 0.0,
-        "subtracted": True,
-        "length": 20,
-    }
-
     waveform = drag_pulse.calculate_waveform()
     assert len(waveform) == 20
     assert isinstance(waveform, np.ndarray)
