f9ab14ef6c537e8144bd246b62a0be78e7aff8cb
Serwan Asaad <serwan.asaad@quantum-machines.co>
2023-10-19 13:00:35 +0200

    add channels

--- quam/components/__init__.py
+++ quam/components/__init__.py
@@ -1,11 +1,13 @@
 from .superconducting_qubits import *
 from .hardware import *
+from .channels import *
 from .quam import *
 from . import pulses
 
 __all__ = [
     *superconducting_qubits.__all__,
     *hardware.__all__,
+    *channels.__all__,
     *quam.__all__,
     "pulses",
 ]
