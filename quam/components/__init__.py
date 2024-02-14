bbacf9bb64e1b582c4f4c94df382efc0ed6af2d7
Serwan Asaad <serwan.asaad@quantum-machines.co>
2024-02-14 15:53:20 +0100

    remove component import

--- quam/components/__init__.py
+++ quam/components/__init__.py
@@ -1,4 +1,3 @@
-from ..examples.superconducting_qubits import components
 from .hardware import *
 from .octave import *
 from .channels import *
@@ -8,6 +7,5 @@ __all__ = [
     *hardware.__all__,
     *channels.__all__,
     *octave.__all__,
-    "components",
     "pulses",
 ]
