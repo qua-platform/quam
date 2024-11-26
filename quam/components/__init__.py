740d590214cc9b06f6ee57f8414f5aef70f7765d
Serwan Asaad <serwan.asaad@quantum-machines.co>
2024-11-26 20:27:10 +0100

    add qubit to init

--- quam/components/__init__.py
+++ quam/components/__init__.py
@@ -3,11 +3,13 @@ from .hardware import *
 from .octave import *
 from .channels import *
 from . import pulses
+from .qubit import *
 
 __all__ = [
     *basic_quam.__all__,
     *hardware.__all__,
     *channels.__all__,
     *octave.__all__,
+    *qubit.__all__,
     "pulses",
 ]
