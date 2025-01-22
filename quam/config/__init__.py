5df9f10cf56ba6fa7500cbc13a4ca03405052497
Maxim V4S <maxim.d@v4scale.com>
2025-01-22 16:26:57 +0300

    feat: add `get_quam_config` to package root

--- quam/config/__init__.py
+++ quam/config/__init__.py
@@ -1,3 +1,4 @@
+from .resolvers import get_quam_config
 from .models import QuamConfig, QuamTopLevelConfig
 
-__all__ = ["QuamConfig", "QuamTopLevelConfig"]
+__all__ = ["QuamConfig", "QuamTopLevelConfig", "get_quam_config"]
