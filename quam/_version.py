48063abeb477dba52e3ba39d26a8f05a34271deb
Serwan Asaad <serwan.asaad@quantum-machines.co>
2025-04-09 15:19:23 +0200

    remove comment

--- quam/_version.py
+++ quam/_version.py
@@ -2,7 +2,6 @@ from importlib.metadata import version, PackageNotFoundError
 
 try:
     # Reads version from installed package metadata.
-    # Replace "quam" with your actual distribution name if different.
     __version__ = version("quam")
 except PackageNotFoundError:
     # Fallback when package is not installed (e.g., running from source)
