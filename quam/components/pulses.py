8646ff7cf3f7b7cd1eafd4931bb0e278f6324734
Serwan Asaad <serwan.asaad@quantum-machines.co>
2026-01-15 21:02:51 +0100

    reintroduce original FlatTopGaussianPulse

--- quam/components/pulses.py
+++ quam/components/pulses.py
@@ -758,6 +758,51 @@ class GaussianPulse(Pulse):
 
 
 @quam_dataclass
+class FlatTopGaussianPulse(Pulse):
+    """Gaussian pulse with flat top QUAM component.
+
+    Args:
+        length (int): The total length of the pulse in samples.
+        amplitude (float): The amplitude of the pulse in volts.
+        axis_angle (float, optional): IQ axis angle of the output pulse in radians.
+            If None (default), the pulse is meant for a single channel or the I port
+                of an IQ channel
+            If not None, the pulse is meant for an IQ channel (0 is X, pi/2 is Y).
+        flat_length (int): The length of the pulse's flat top in samples.
+            The rise and fall lengths are calculated from the total length and the
+            flat length.
+    """
+
+    amplitude: float
+    axis_angle: float = None
+    flat_length: int
+
+    def waveform_function(self):
+        from qualang_tools.config.waveform_tools import flattop_gaussian_waveform
+
+        rise_fall_length = (self.length - self.flat_length) // 2
+        if not self.flat_length + 2 * rise_fall_length == self.length:
+            raise ValueError(
+                "FlatTopGaussianPulse rise_fall_length (=length-flat_length) must be"
+                f" a multiple of 2 ({self.length} - {self.flat_length} ="
+                f" {self.length - self.flat_length})"
+            )
+
+        waveform = flattop_gaussian_waveform(
+            amplitude=self.amplitude,
+            flat_length=self.flat_length,
+            rise_fall_length=rise_fall_length,
+            return_part="all",
+        )
+        waveform = np.array(waveform)
+
+        if self.axis_angle is not None:
+            waveform = waveform * np.exp(1j * self.axis_angle)
+
+        return waveform
+
+
+@quam_dataclass
 class _FlatTopGaussianPulse(Pulse):
     """_FlatTopGaussianPulse QUAM component.
 
