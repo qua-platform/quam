a00c07d83e73cb2d3deaa2c6d74c858274528c40
MichalGoldenshtein <michal@quantum-machines.co>
2026-01-26 23:10:36 -0600

    adding quam cavity component

--- quam/components/quantum_components/__init__.py
+++ quam/components/quantum_components/__init__.py
@@ -1,5 +1,6 @@
 from quam.components.quantum_components.quantum_component import QuantumComponent
 from quam.components.quantum_components.qubit import Qubit
 from quam.components.quantum_components.qubit_pair import QubitPair
+from quam.components.quantum_components.cavity import Cavity
 
-__all__ = ["QuantumComponent", "Qubit", "QubitPair"]
+__all__ = ["QuantumComponent", "Qubit", "QubitPair", "Cavity"]
